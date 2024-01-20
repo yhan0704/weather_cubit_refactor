f16fffc3a2ab9f528897d0004d3fae5a02320021
yhan0704 <hanyc1205@gmail.com>
2024-01-19 19:00:40 -0500

    removed subscription and used BlocListener

--- lib/cubits/theme/theme_cubit.dart
+++ lib/cubits/theme/theme_cubit.dart
@@ -1,35 +1,17 @@
-import 'dart:async';
-
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
-
-import '../../constants/constants.dart';
-import '../weather/weather_cubit.dart';
+import 'package:open_weather_cubit_listener/constants/constants.dart';
 
 part 'theme_state.dart';
 
 class ThemeCubit extends Cubit<ThemeState> {
-  late final StreamSubscription weatherSubscription;
+  ThemeCubit() : super(ThemeState.initial());
 
-  final WeatherCubit weatherCubit;
-  ThemeCubit({
-    required this.weatherCubit,
-  }) : super(ThemeState.initial()) {
-    weatherSubscription =
-        weatherCubit.stream.listen((WeatherState weatherState) {
-      print('weatherState: $weatherState');
-
-      if (weatherState.weather.temp > kWarmOrNot) {
-        emit(state.copyWith(appTheme: AppTheme.light));
-      } else {
-        emit(state.copyWith(appTheme: AppTheme.dark));
-      }
-    });
-  }
-
-  @override
-  Future<void> close() {
-    weatherSubscription.cancel();
-    return super.close();
+  void setTheme(double currentTemp) {
+    if (currentTemp > kWarmOrNot) {
+      emit(state.copyWith(appTheme: AppTheme.light));
+    } else {
+      emit(state.copyWith(appTheme: AppTheme.dark));
+    }
   }
 }
